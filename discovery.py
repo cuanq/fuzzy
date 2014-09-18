24e606e6e9a572c54c1176412c34024d56575ac4
Danielle Gonzalez <dng2551@rit.edu>
2014-09-18 14:27:01 -0400

    implementing link discovery with Beautiful Soup

--- discovery.py
+++ discovery.py
@@ -1,26 +1,30 @@
 import requests
 import sys
+from bs4 import BeautifulSoup
 
 class discovery:
-    def __init__(self):
-        pass()
-    
+
     # Fuzzer must crawl and guess pages
     # Includes Link discovery & Page guessing
-    def discoverPage():
-        pass()
+    def discoverPage(page):
+        pass
    
    # Fuzzer should keep a list of URLs that it can reach from init page
    # no off-site links
-    def discoverLink():
-        pass()
+    def discoverLink(page):
+        linksFound = []
+
+        aPage = requests.get(page)
+        pageData = aPage.text
+        pageSoup = BeautifulSoup(pageData)
+        for link in pageSoup.find_all('a'):
+            linksFound.append(link.get('href'))
 
     # Fuzzer should use common word list to discover potenially unlinked pages
     def guessPage():
-        pass()
+        pass
     # Fuzzer should attempt to discover every possible input into system
     # Includes Parse URLs, Form parameters, & Cookies 
     def discoverInput():
-        pass()
 
-        
+        pass
