f042ef041c08a17a62cea62c4d7584d74363fb9c
Danielle Gonzalez <dng2551@rit.edu>
2014-09-18 16:07:12 -0400

    discovery is no longer a class

--- discovery.py
+++ discovery.py
@@ -4,33 +4,31 @@ from urlparse import urljoin
 from urlparse import urlparse
 from bs4 import BeautifulSoup
 
-class discovery:
+# Fuzzer must crawl and guess pages
+# Includes Link discovery & Page guessing
+def discoverPage(page):
+    pass
 
-    # Fuzzer must crawl and guess pages
-    # Includes Link discovery & Page guessing
-    def discoverPage(page):
-        pass
-   
-   # Fuzzer should keep a list of URLs that it can reach from init page
-   # no off-site links
- 
-    def discoverLink(page):
-        linksFound = []
+# Fuzzer should keep a list of URLs that it can reach from init page
+# no off-site links
 
-        parsed_url = urlparse(page)
-        site = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_url)
+def discoverLink(page):
+    linksFound = []
 
-        page = requests.get(url)
-        pageData = page.text
-        pageSoup = BeautifulSoup(pageData)
-        for link in pageSoup.find_all('a'):
-            if (link.get('href')).startswith(site):
-                linksFound.append(link.get('href'))
-            else:
-                linksFound.append(site + link.get('href'))
+    parsed_url = urlparse(page)
+    site = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_url)
 
-        return linksFound
+    page = requests.get(url)
+    pageData = page.text
+    pageSoup = BeautifulSoup(pageData)
+    for link in pageSoup.find_all('a'):
+        if (link.get('href')).startswith(site):
+            linksFound.append(link.get('href'))
+        else:
+            linksFound.append(site + link.get('href'))
 
-    # Fuzzer should use common word list to discover potenially unlinked pages
-    def guessPage():
-        pass
+    return linksFound
+
+# Fuzzer should use common word list to discover potenially unlinked pages
+def guessPage():
+    pass
