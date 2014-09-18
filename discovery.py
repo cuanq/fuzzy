17c293d643d30f222b492368340610298daab6cb
Danielle Gonzalez <dng2551@rit.edu>
2014-09-18 14:04:43 -0400

    initial discovery.py shell

---
+++ discovery.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+class discovery:
+    def __init__(self):
+        pass()
+    
+    # Fuzzer must crawl and guess pages
+    # Includes Link discovery & Page guessing
+    def discoverPage():
+        pass()
+   
+   # Fuzzer should keep a list of URLs that it can reach from init page
+   # no off-site links
+    def discoverLink():
+        pass()
+
+    # Fuzzer should use common word list to discover potenially unlinked pages
+    def guessPage():
+        pass()
+    # Fuzzer should attempt to discover every possible input into system
+    # Includes Parse URLs, Form parameters, & Cookies 
+    def discoverInput():
+        pass()
+
+        
