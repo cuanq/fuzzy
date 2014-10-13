8d7de8ed3c093e8c0f0ead89c63dd1b2acad6ba3
Danielle Gonzalez <dng2551@rit.edu>
2014-10-13 17:53:28 -0400

    forgot an import in httpresponse

--- httpresponse.py
+++ httpresponse.py
@@ -3,6 +3,7 @@ File: httpresponse.py
 Authors: Zack Downs <zjd2035@gmail.com>, Danielle Gonzalez <dng2551@rit.edu>, Stephan Wlodarczyk <stephanjwlodarczyk@gmail.com>
 """
 import requests 
+import random
 
 class HttpResponse():
 
