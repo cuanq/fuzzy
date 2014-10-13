a32befff7ee8c2a7a3fe81c64bda46cec2aab38a
StephanJW <stephanjwlodarczyk@gmail.com>
2014-10-13 16:24:29 -0400

    Modified test.py to incorporate reading of dictionary items

--- test.py
+++ test.py
@@ -4,24 +4,22 @@ Authors: Zack Downs <zjd2035@gmail.com>, Danielle Gonzalez <dng2551@rit.edu>, St
 """
 import random
 
-
 """ 
 Takes in an assortment of web urls, and user enabled 
 testing options (i.e. vectors, sensititive, random, slow)
 """
-def test_pages(url, testing_options):
+def test_pages(forms, testing_options):
 
 	""" Setup strategy """
-	if testing_option.lower() == "true"
+	if testing_options["random"].lower() == "true"
 		print("random shuffling enabled")
 		random.shuffle(url)
 
-	sanitize = VulnerabilityStrategy(url, Sanitization(), testing_options)
-	delayResponse = VulnerabilityStrategy(url, DelayReponse(), testing_options)
-	httpResponse = VulnerabilityStrategy(url, HttpResponse(), testing_options)
-	sensistive = VulnerabilityStrategy(url, SensitiveData(), testing_options)
+	sanitize = VulnerabilityStrategy(forms, Sanitization(), testing_options)
+	delayResponse = VulnerabilityStrategy(forms, DelayReponse(), testing_options)
+	httpResponse = VulnerabilityStrategy(forms, HttpResponse(), testing_options)
+	sensistive = VulnerabilityStrategy(forms, SensitiveData(), testing_options)
 
-	""" Execute vulernability strategies """
 	sanitize.run()
 	delayResponse.run()
 	httpResponse.run()
